e531cc3fe89ab160d7deb53b25640e0f3debe453
ehsanmorgan <ehsanmorgan1994@gmail.com>
2023-03-23 23:18:51 +0100

    -

--- transaltion/models.py
+++ transaltion/models.py
@@ -39,33 +39,3 @@ class Questions(models.Model):
         
         
         
-class Winery(models.Model):
-    class Meta:
-        verbose_name = _('Winery')
-        verbose_name_plural = _('Winery-plural')
-        ordering = ('sort', )
-
-    title = models.CharField(_('Title'), max_length=250, default='', blank=True, null=False)
-    description = models.TextField(_('Description'), null=True, blank=True)
-    sort = models.IntegerField(_('Sort'), default=0, blank=True, null=True)
-
-
-class Wine(models.Model):
-    class Meta:
-        verbose_name = _('wine')
-        verbose_name_plural = _('wine-plural')
-        ordering = ('sort', )
-
-    title = models.CharField(_('Title'), max_length=250, default='', blank=True, null=False)
-    description = models.TextField(_('Description'), null=True, blank=True)
-    active = models.BooleanField(_('Active'), default=False)
-    sort = models.IntegerField(_('Sort'), default=0, blank=True, null=True)
-
-    temperature_from = models.FloatField(_('Temperature_from'), help_text=_('in ° C'), null=True, blank=True)
-    temperature_to = models.FloatField(_('Temperature_to'), help_text=_('in ° C'), null=True, blank=True)
-
-
-class Vintage(models.Model):
-    wine = models.ForeignKey(Wine, related_name='vintages', on_delete=models.CASCADE)
-    year = models.IntegerField(_('Year'), default=0)
-    description = models.TextField(_('Description'), null=True, blank=True)
