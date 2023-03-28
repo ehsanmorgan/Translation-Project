f22bef972e95c9a2cf798c7f96fa8a6e2d8bf3ee
ehsanmorgan <ehsanmorgan1994@gmail.com>
2023-03-28 19:42:57 +0200

    -

--- transaltion/admin.py
+++ transaltion/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from tof.admin import TofAdmin, TranslationTabularInline
-from tof.decorators import tof_prefetch
+#from tof.admin import TofAdmin, TranslationTabularInline
+#from tof.decorators import tof_prefetch
 from .models import Questions
 from modeltranslation.admin import TranslationAdmin
 
@@ -10,7 +10,7 @@ class adminquestion(TranslationAdmin):
     pass
 
 
-admin.site.register(Questions,adminquestion)
+admin.site.register(Questions)
 
     
     
--- transaltion/translation.py
+++ transaltion/translation.py
@@ -1,8 +1,12 @@
-from modeltranslation.translator import TranslationOptions,register
+from modeltranslation.translator import translator, TranslationOptions
 from .models import Questions
 
 
 
-@register(Questions)
+
 class questionsTranslation(TranslationOptions):
     fields=('question','first_option','second_option','third_option')
+    
+    
+    
+translator.register(Questions, questionsTranslation)
