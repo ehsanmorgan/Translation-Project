7221a5b7dacdaa6d0be1cc926b99e57b71d9125e
ehsanmorgan <ehsanmorgan1994@gmail.com>
2023-04-02 19:02:28 +0200

    '

--- transaltion/admin.py
+++ transaltion/admin.py
@@ -13,8 +13,18 @@ from modeltranslation.admin import TranslationAdmin
 
 
 class adminquestion(TranslationAdmin):
-    pass
+    list_display=('first_option')
+    group_fieldsets = True
 
+    class Media:
+        js = (
+            'http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js',
+            'http://ajax.googleapis.com/ajax/libs/jqueryui/1.10.2/jquery-ui.min.js',
+            'modeltranslation/js/tabbed_translation_fields.js',
+        )
+        css = {
+            'screen': ('modeltranslation/css/tabbed_translation_fields.css',),
+        }
 
 admin.site.register(Questions)
 
