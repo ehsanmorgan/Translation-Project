2a6623d670ca581f51a141f1c9e33a7dcf5185a5
ehsanmorgan <ehsanmorgan1994@gmail.com>
2023-04-06 18:43:57 +0200

    -

--- project/settings.py
+++ project/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tof',
     'django_social_share',
     'modeltranslation',
     'django.contrib.admin',
