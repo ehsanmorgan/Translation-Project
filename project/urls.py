f3937c07222d151aeb06def8b1b88edd5a637d5c
ehsanmorgan <ehsanmorgan1994@gmail.com>
2023-05-06 19:08:27 +0200

    -

--- project/urls.py
+++ project/urls.py
@@ -33,6 +33,8 @@ urlpatterns = [
 
 
 
+
+
 urlpatterns=[
        *i18n_patterns(*urlpatterns, prefix_default_language=False),
         path("set_language/<str:language>", set_language, name="set-language"),
